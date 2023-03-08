09c54486e2e5b00a6d807b95384058534371d359
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-03-07 22:09:02 -0300

    feat: configurando db_context

---
+++ src/API/SuperGym/SuperGym.Infra/Context/SuperGymDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace SuperGym.Infra.Context;
+public class SuperGymDbContext : DbContext
+{
+    public SuperGymDbContext(DbContextOptions<SuperGymDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
+
