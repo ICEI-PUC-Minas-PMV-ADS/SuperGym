39415a1106753bedf04e096f501507aecefa31f5
Rafael Jordão <rafaeljordao.picado@gmail.com>
2023-03-08 14:06:03 -0300

    Criando Mocks

---
+++ src/app/src/mocks/exercises.ts
@@ -0,0 +1,34 @@
+export const exercises = [
+  {
+    _id: 'f00f3a68-bc84-4363-bb4f-3e3e27b0aafb',
+    name: 'Agachamento',
+    description: 'Exercício para pernas e glúteos',
+    series: 3,
+    waitTime: '30 a 60s',
+    imagePath: '',
+    instructions: [
+      'Posicione os pés na largura dos ombros',
+      'Flexione os joelhos e empurre os quadris para trás',
+      'Mantenha o peito erguido e a coluna reta',
+      'Abaixe até que as coxas fiquem paralelas ao chão',
+      'Empurre através dos calcanhares e levante-se'
+    ],
+    category: '5ea80343-ae9e-447e-b30a-8496e53d90b6'
+  },
+  {
+    _id: 'f00f3a68-bc84-4363-bb4f-3e3e27b0aafb',
+    name: 'Supino',
+    description: 'Exercício para o peitoral',
+    series: 3,
+    waitTime: '30 a 60s',
+    imagePath: '',
+    instructions: [
+      'Posicione os pés na largura dos ombros',
+      'Flexione os joelhos e empurre os quadris para trás',
+      'Mantenha o peito erguido e a coluna reta',
+      'Abaixe até que as coxas fiquem paralelas ao chão',
+      'Empurre através dos calcanhares e levante-se'
+    ],
+    category: '5ea80343-ae9e-447e-b30a-8496e53d90b6',
+  },
+]
