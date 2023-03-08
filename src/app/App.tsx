0d176b9cfd75d05b94c497235d9418c76e6b08c6
Rafael Jordão <rafaeljordao.picado@gmail.com>
2023-03-08 17:30:26 -0300

    Criando componente de Texto

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,15 @@
 import { useFonts } from 'expo-font';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text } from './src/components/Text';
 
 
 export default function App() {
   const [isFontsLoaded] = useFonts({
     'Poppins-400': require('./src/assets/fonts/Poppins-Regular.otf'),
-    'Poppins-500': require('./src/assets/fonts/Poppins-SemiBold.otf'),
-    'Poppins-600': require('./src/assets/fonts/Poppins-Bold.otf'),
+    'Poppins-600': require('./src/assets/fonts/Poppins-SemiBold.otf'),
+    'Poppins-700': require('./src/assets/fonts/Poppins-Bold.otf'),
   });
 
   if (!isFontsLoaded) {
@@ -17,7 +18,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontFamily: 'Poppins-600' }}>Fontes configuradas com sucesso!</Text>
+      <Text color="#25C26E" weight="600" size={22}>Setup Inicial - Supergym</Text>
+      <Text color="#333" weight="400" size={16}>Componente de texto criado</Text>
       <StatusBar style="auto" />
     </View>
   );
