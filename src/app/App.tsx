aaa90e1a648b2756d2bea1f8800417f4d097ed51
Rafael Jordão <rafaeljordao.picado@gmail.com>
2023-03-08 17:12:34 -0300

    Adicionando Família de Fontes - Poppins

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,23 @@
+import { useFonts } from 'expo-font';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'Poppins-400': require('./src/assets/fonts/Poppins-Regular.otf'),
+    'Poppins-500': require('./src/assets/fonts/Poppins-SemiBold.otf'),
+    'Poppins-600': require('./src/assets/fonts/Poppins-Bold.otf'),
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Poppins-600' }}>Fontes configuradas com sucesso!</Text>
       <StatusBar style="auto" />
     </View>
   );
