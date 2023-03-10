b9afc75d73767babcbf2aa833f2b0a932d2dcf6c
TioTaylor <1083465@sga.pucminas.br>
2023-03-09 21:00:52 -0300

    Pull Pasta App

---
+++ src/app/src/mocks/categories.ts
@@ -0,0 +1,12 @@
+export const categories = [
+  {
+    _id: '5ea80343-ae9e-447e-b30a-8496e53d90b6',
+    name: 'Musculação',
+    icon: '💪'
+  },
+  {
+    _id: 'efe67569-c408-4f4d-afa5-bdb6a0ad2d3c',
+    name: 'Cardio',
+    icon: '🫀'
+  },
+]
