71c851a08835bea669b02ba1ac7bf2db5e8a5315
Rafael Jordão <rafaeljordao.picado@gmail.com>
2023-03-13 12:04:30 -0300

    Estrutura da página Main

--- src/app/src/pages/Main/index.tsx
+++ src/app/src/pages/Main/index.tsx
@@ -3,18 +3,32 @@ import React from 'react';
 
 import { Header } from '../../components/Header';
 
-import { Container, CategoriesContainer, ExercisesContainer, FooterContainer } from './styles';
+import {
+  Container,
+  CategoriesContainer,
+  ExercisesContainer,
+  Footer,
+  FooterContainer
+} from './styles';
 
 function Main() {
 
 
   return (
-    <Container>
-      <Header />
-      <CategoriesContainer></CategoriesContainer>
-      <ExercisesContainer></ExercisesContainer>
-      <FooterContainer></FooterContainer>
-    </Container>
+    <>
+      <Container>
+        <Header />
+
+        <CategoriesContainer></CategoriesContainer>
+
+        <ExercisesContainer></ExercisesContainer>
+      </Container>
+
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
+    </>
+
   );
 }
 
--- src/app/src/pages/Main/styles.ts
+++ src/app/src/pages/Main/styles.ts
@@ -5,13 +5,25 @@ const isAndroid = Platform.OS === 'android';
 
 export const Container = styled.SafeAreaView`
   margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0'};
+  flex: 1;
+  background: #fafafa;
+;
 `;
 
 export const CategoriesContainer = styled.View`
+  height: 73px;
+  margin-top: 34px;
 `;
 
 export const ExercisesContainer = styled.View`
+  height: 50px;
+  flex: 1;
 `;
 
-export const FooterContainer = styled.View`
+export const Footer = styled.View`
+  background: white;
+  min-height: 110px;
+`;
+
+export const FooterContainer = styled.SafeAreaView`
 `;
