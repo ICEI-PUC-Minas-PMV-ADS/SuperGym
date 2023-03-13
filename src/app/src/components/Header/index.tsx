20d543ce0a3b8730d4f0cd650181a9cf175bc923
Rafael Jordão <rafaeljordao.picado@gmail.com>
2023-03-13 11:41:58 -0300

    Iniciando estrutura de componentes

---
+++ src/app/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+import { Text } from '../../components/Text';
+
+export function Header() {
+  return (
+    <Container>
+      <Text>Bem-vindo(a) ao</Text>
+      <Text>SUPERGYM</Text>
+    </Container>
+  );
+}
---
+++ src/app/src/pages/Main/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import { Header } from '../../components/Header';
+
+import { Container } from './styles';
+
+function Main() {
+
+
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default Main;
