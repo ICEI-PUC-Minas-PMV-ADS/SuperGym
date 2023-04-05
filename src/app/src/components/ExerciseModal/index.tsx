65bbe22535fd4799076f44dfb484c95bf30f2709
Rafael Jordão <rafaeljordao.picado@gmail.com>
2023-04-05 11:09:20 -0300

    Adicionando imagem aos detalhes do exercício

--- src/app/src/components/ExerciseModal/index.tsx
+++ src/app/src/components/ExerciseModal/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Modal } from 'react-native';
 import { Exercise } from '../../types/Exercise';
 import { Text } from '../Text';
+import { Image, CloseButton } from './styles';
+import { Close } from '../Icons/Close';
 
 interface ExerciseModalProps {
   visible: boolean;
@@ -10,7 +12,8 @@ interface ExerciseModalProps {
 }
 
 export function ExerciseModal({ visible, onClose, exercise }: ExerciseModalProps) {
-  console.log(exercise);
+
+
   return (
     <Modal
       visible={visible}
@@ -18,6 +21,13 @@ export function ExerciseModal({ visible, onClose, exercise }: ExerciseModalProps
       presentationStyle='pageSheet'
       onRequestClose={onClose}
     >
+      <Image source={{
+        uri: 'https://images.unsplash.com/photo-1532029837206-abbe2b7620e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      }}>
+        <CloseButton>
+          <Close />
+        </CloseButton>
+      </Image>
       <Text>Hello Modal</Text>
     </Modal>
   );
--- src/app/src/components/ExerciseModal/styles.ts
+++ src/app/src/components/ExerciseModal/styles.ts
@@ -1,1 +1,17 @@
 import styled from 'styled-components/native';
+
+export const Image = styled.ImageBackground`
+  width: 100%;
+  height: 200px;
+  align-items: flex-end
+`;
+
+export const CloseButton = styled.TouchableOpacity`
+  width: 32px;
+  height: 32px;
+  background: rgba(0,0,0, .5);
+  border-radius: 16px;
+  align-items: center;
+  justify-content:center;
+  margin: 24px;
+`;
